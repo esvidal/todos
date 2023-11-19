86fd1f6c136197e7c490d11d153792584863095e
vidal <vidal@vdal.es>
2023-11-19 06:47:04 -0800

    AddTask()

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function myFunc() {
-    alert("This is just a test");
+function addTask() {
+    var input = document.getElementById('taskInput');
+    var newTask = input.value;
+    if (newTask != "") {
+        var li = document.createElement("li");
+        li.textContent = newTask;
+        li.onclick = function() {
+            this.parentNode.removeChild(this);
+        };
+        document.getElementById('tasksList').appendChild(li);
+        input.value="";
+    }
 }
